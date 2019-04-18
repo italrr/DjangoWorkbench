ce0ad644b66298b307567b2cf476c98842c02341
Italo Russo <italrr@gmail.com>
2019-04-18 17:20:05 -0400

    Added Ingredients view

--- WorkbenchApp/models.py
+++ WorkbenchApp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Ingredient(models.Model):
-    # type will serve as the type as well as the name of the ingredient 
-    type = models.CharField(max_length=200, unique=True) # wood, iron, etc
+    name = models.CharField(max_length=100) # display name
+    type = models.CharField(max_length=100, unique=True) # wood, iron, etc
 
 class IngredientInventory(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
--- WorkbenchApp/views.py
+++ WorkbenchApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/home.html')
+
+
+def ingredients(request):
+    return render(request, 'ingredients/ingredients.html')
